16749e0c5fd950841816df31401ee757a250c696
Olumide Bambe <loyalsfc@gmail.com>
2023-09-30 06:52:58 +0100

    integrated contents

--- popup.js
+++ popup.js
@@ -1,123 +1,31 @@
 const startButton = document.getElementById("startButton");
 const stopButton = document.getElementById("stopButton");
 
-let mediaRecorder;
-let recorder;
-let recordedChunks = [];
 let recordType = 'current_tab';
 let isVideoEnabled = true;
 let isMicrophoneEnabled = true;
 let timer;
 
 startButton.addEventListener("click", async () => {
-    recordType === "current_tab" ? captureTab() : captureScreen();
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.tabCapture.getMediaStreamId({consumerTabId: tab.id}, async (streamId) => {
+    const response = await chrome.tabs.sendMessage(tab.id, {recordType, streamId, tabId: tab.id});
+    console.log(response);
+  });
+  
+
+  // chrome.tabs.query({ currentWindow: true, active: true }, function (e) {
+  //   var n = e[0];
+  //   chrome.tabs.sendMessage(n.id, {
+  //     message: 'get-page-title',
+  //   }).then((response) => {
+  //     console.log(response.pageTitle);
+  //   });
+      // chrome.tabCapture.getMediaStreamId({consumerTabId: n.id}, (streamId) => {
+      //   chrome.tabs.sendMessage(n.id, {type: recordType, streamId:streamId, tabId: n.id});
+      // });
+  // });
 });
-
-function captureTab(){
-  chrome.tabCapture.capture({
-    video: isVideoEnabled,
-    audio: isMicrophoneEnabled,
-  }, function(stream) {
-    const output = new AudioContext();
-    const source = output.createMediaStreamSource(stream);
-    source.connect(output.destination);
-    recordTab(stream);
-  });
-}
-
-async function captureScreen(){
-  // const stream = await navigator.mediaDevices.getDisplayMedia({ video: { mediaSource: "screen" } });
-  chrome.desktopCapture.chooseDesktopMedia(["screen"], function(stream) {
-    // Get a list of all audio input devices.
-    chrome.mediaDevices.enumerateDevices(function(devices) {
-      // Select the desired audio input device from the list.
-      var audioInputDevice = devices.find(device => device.kind === "audioinput");
-
-      // Create a MediaRecorder object and pass it the video source and the audio source.
-      var recorder = new MediaRecorder(stream, { audio: audioInputDevice });
-
-      // Start recording.
-      recorder.start();
-
-      // Stop recording after 10 seconds.
-      setTimeout(function() {
-        recorder.stop();
-      }, 5000);
-
-      // Get the recorded data.
-      recorder.onstop = function(event) {
-        // Do something with the recorded data, such as saving it to a file or uploading it to a server.
-        console.log(event)
-      };
-    });
-  });
-
-}
-
-function recordTab(stream){
-  recorder = new MediaRecorder(stream);
-
-  recorder.ondataavailable = (event) => {
-    console.log('data size available guys')
-    if (event.data.size > 0) {
-      recordedChunks.push(event.data);
-    }
-  };
-
-  // Start recording.
-  recorder.start();
-
-  // Stop recording after 5 seconds.
-  setTimeout(function() {
-    recorder.stop();
-    stopTimer()
-  }, 5000);
-
-  // Get the recorded data.
-  recorder.onstop = function(event) {
-    processRecording(recordedChunks);
-    stopTimer();
-  };
-
-  recorder.onstart = () => {
-    recordCounter();
-  }
-}
-
-function recordScreen(stream){
-  mediaRecorder = new MediaRecorder(stream);
-
-  mediaRecorder.ondataavailable = (event) => {
-    console.log('data size available guys')
-    if (event.data.size > 0) {
-      recordedChunks.push(event.data);
-    }
-  };
-
-  mediaRecorder.onstop = () => {
-    processRecording(recordedChunks)
-  };
-
-  mediaRecorder.onstart = () => {
-    recordCounter();
-  }
-
-  mediaRecorder.start();
-  startButton.disabled = true;
-  stopButton.disabled = false;
-}
-
-function processRecording(recordedChunks){
-  const blob = new Blob(recordedChunks, { type: "video/webm" });
-  const url = window.URL.createObjectURL(blob);
-  const a = document.createElement("a");
-  a.style.display = "none";
-  a.href = url;
-  a.download = "screen-recording.webm";
-  document.body.appendChild(a);
-  a.click();
-  window.URL.revokeObjectURL(url);
-}
 
 stopButton.addEventListener("click", () => {
   mediaRecorder.stop();
@@ -147,27 +55,23 @@ document.getElementById('microphone-toggle').addEventListener('change', (event) 
   isMicrophoneEnabled = event.target.checked
 })
 
-function recordCounter(){
-  let hours = 0;
-  let minutes = 0;
-  let seconds = 1;
-  timer = setInterval(function () {
-    seconds++;
-    if (seconds >= 60) {
-        seconds = 0;
-        minutes++;
-        if (minutes >= 60) {
-            minutes = 0;
-            hours++;
-        }
-    }
 
-    // Format the time as HH:MM:SS
-    let formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-    document.getElementById('current-time').textContent = formattedTime;
-  }, 1000);
-}
 
-function stopTimer() {
-  clearInterval(timer);
-}
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   document.getElementById('startRecording').addEventListener('click', function() {
+//       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//           chrome.tabs.executeScript(
+//               tabs[0].id,
+//               {code: `
+//                   ${startScreenRecording}
+//                   startScreenRecording();
+//               `}
+//           );
+//       });
+//   });
+// });
